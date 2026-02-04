1c4ff2d3225a6ab49cac80e5a1348781a3c89a28
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 11:17:32 -0400

    feat: operador aritmetico

---
+++ Modulo2/OperadorAritmetico1.java
@@ -0,0 +1,14 @@
+public class OperadorAritmetico1 {
+    public static void main(String[] args) {
+        int notaAluno1 = 88;
+        int notaAluno2 = 75;
+        int notaAluno3 = 99;
+
+        int totalGeral = notaAluno1 + notaAluno2 + notaAluno3;
+        System.out.println("Total geral: " + totalGeral);
+
+        int mediaGeral = (notaAluno1 + notaAluno2 + notaAluno3) / 3;
+        System.out.println("Media geral: " + mediaGeral);
+
+    }
+}
