5bf0614f5c92cc139d46ef2ba151faeb940520ff
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 12:01:55 -0400

    feat: conversao de tipo primitivo
    - Usando cast

---
+++ Modulo2/ConversaoTipoPrimitivo.java
@@ -0,0 +1,9 @@
+public class ConversaoTipoPrimitivo {
+    public static void main(String[] args) {
+        long x = 10;
+
+        // casting
+        int y = (int) x;
+        System.out.println(y);
+    }
+}
