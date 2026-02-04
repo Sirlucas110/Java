c81b817590fcb5a8eace374cca0e8c485266d295
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 12:08:14 -0400

    feat: promoção aritmetica

---
+++ Modulo2/PromocaoAritmetica.java
@@ -0,0 +1,9 @@
+public class PromocaoAritmetica {
+
+    public static void main(String [] args) {
+        long x = 10;
+        float y = 5.6F;
+        float z = x / y;
+        System.out.println(z);
+    }
+}
