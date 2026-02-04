d4c4e60261bf2e429b29915ffe57123dfae46099
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 00:04:39 -0400

    feat: comentário no java

--- Modulo2/OiMundo.java
+++ Modulo2/OiMundo.java
@@ -1,6 +1,11 @@
 public class OiMundo {
 
 	public static void main(String[] args) {
+		/*
+		Está linha ignorada pelo compilador
+		System.out.println("essa instrução será ignorada");
+		e essa também
+		*/
 	    System.out.println("Oi Mundo");
 	}
 
