f3eb92e4588b31bfa491937c394eb1ab3f6267ab
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 12:54:01 -0400

    feat: trabalhando com strings

---
+++ Modulo2/TrabalhandoComStrings.java
@@ -0,0 +1,8 @@
+public class TrabalhandoComStrings {
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        int idade = 30;
+
+        System.out.println(nome + " tem " + idade + " anos");
+    }
+}
