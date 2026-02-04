f27b28b901ec06cdd697d9ccbbb5199b98d644c2
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 10:46:56 -0400

    feat: sequencia de escape

--- Modulo2/OiMundo.java
+++ Modulo2/OiMundo.java
@@ -1,12 +1,7 @@
 public class OiMundo {
 
 	public static void main(String[] args) {
-		/*
-		Está linha ignorada pelo compilador
-		System.out.println("essa instrução será ignorada");
-		e essa também
-		*/
-	    System.out.println("Oi Mundo");
+	    System.out.println("Oi \"Mundo\"");
 	}
 
 }
