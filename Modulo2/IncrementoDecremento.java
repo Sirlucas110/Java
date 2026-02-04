03f65ed8a29587398bc995758a9d70cee3d6ec36
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-04 16:08:22 -0400

    feat: incremento e decremento

---
+++ Modulo2/IncrementoDecremento.java
@@ -0,0 +1,9 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int idade = 10;
+
+        int novaIdade = ++idade;
+        // int novaIdade = --idade;
+        System.out.println(novaIdade);
+    }
+}
