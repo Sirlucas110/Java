90bea2f5ddbaeb927e531ce141c9ffcfa47f7f0c
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-05 13:19:11 -0400

    feat: programinha de estoque com arrays

---
+++ POO/src/Modulo4/Principal.java
@@ -0,0 +1,35 @@
+package Modulo4;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Quantos produtos? ");
+		int quantidadeProdutos = entrada.nextInt();
+		
+		entrada.nextLine();
+		
+		Estoque estoque = new Estoque();
+		
+		estoque.produtos = new Produto[quantidadeProdutos];
+		
+		for (int i = 0; i < estoque.produtos.length; i++) {
+			estoque.produtos[i] = new Produto();
+			
+			System.out.println("\nProduto " + i);
+			System.out.println("------------------------");
+			
+			System.out.print("Descrição:");
+			estoque.produtos[i].descricao = entrada.nextLine();
+			
+			System.out.print("Quantidade de itens:");			
+			estoque.produtos[i].quantidade = entrada.nextInt();
+			entrada.nextLine();
+		}
+		
+		estoque.listarProdutos();
+		
+	}
+}
---
+++ POO/src/Modulo4/Produto.java
@@ -0,0 +1,10 @@
+package Modulo4;
+
+public class Produto {
+	String descricao;
+	int quantidade;
+	
+	void descrever() {
+		System.out.println(descricao + " - " + quantidade + " itens");
+	}
+}
