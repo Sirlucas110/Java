91c3491990c0d7270f8445106d4ab115c70854d2
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-05 17:42:44 -0400

    feat: programa usando java bean e getters e setters

---
+++ POO/src/Modulo5/Principal.java
@@ -0,0 +1,27 @@
+package Modulo5;
+
+public class Principal {
+	public static void main(String[] args) {
+		Fornecedor imobiliaria = new Fornecedor();
+		
+		imobiliaria.setNome("Negócios imobiliários");
+		
+		Fornecedor mercado = new Fornecedor();
+		mercado.setNome("Mercado do Lucas");
+		
+		ContaPagar conta1 = new ContaPagar();
+		conta1.setDescricao("Aluguel da matriz");
+		conta1.setValor(1230d);
+		conta1.setDataVencimento("05/02/2026");
+		conta1.setFornecedor(imobiliaria);
+		
+		
+		ContaPagar conta2 = new ContaPagar(mercado, "Compra do mês", 200, "05/02/2026");
+		
+		ContaPagar conta3 = new ContaPagar(mercado, "Parcela presente", 68, "10/05/2026");
+		
+		conta1.pagar();
+		conta2.pagar();
+		conta3.pagar();
+	}
+}
