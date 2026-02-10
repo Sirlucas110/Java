5ec9d2c0b84c3d389e36b4f87b78cccf6312f69f
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-10 15:38:08 -0400

    feat: sobreposicao

---
+++ POO/src/com/poo/sobreposicao/Produto.java
@@ -0,0 +1,9 @@
+package com.poo.sobreposicao;
+
+public class Produto {
+	protected String descricao;
+	
+	public void identificar() {
+		System.out.println("Minha descrição é " + descricao + ". ");
+	}
+}
