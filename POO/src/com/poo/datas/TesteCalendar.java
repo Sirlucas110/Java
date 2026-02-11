b297a84e431ef2d4fff8be6ab9123c08313285cf
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-11 16:10:01 -0400

    feat: datas

---
+++ POO/src/com/poo/datas/TesteCalendar.java
@@ -0,0 +1,25 @@
+package com.poo.datas;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class TesteCalendar {
+	public static void main(String[] args) {
+		Calendar c = new GregorianCalendar();
+		
+		c.set(Calendar.DAY_OF_MONTH, 15);
+		c.set(Calendar.MONTH, 8);
+		c.set(Calendar.YEAR, 2023);
+		c.set(Calendar.HOUR_OF_DAY, 16);
+		c.set(Calendar.MINUTE, 30);
+		
+		DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println(formatador.format(c.getTime()));
+		c.add(Calendar.DAY_OF_MONTH, -1);
+		System.out.println(formatador.format(c.getTime()));
+		
+	}
+}
