fa8ca54c941bf3fd65a33792b5dcce16a32db547
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-11 16:10:12 -0400

    feat: desafio datas

---
+++ POO/src/com/poo/gravidez/CalculadoraGravidez.java
@@ -0,0 +1,32 @@
+package com.poo.gravidez;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalculadoraGravidez {
+
+	private Date dataUltimoPeriodoMenstrual;
+
+	public CalculadoraGravidez(Date dataUltimoPeriodoMenstrual) {
+		this.dataUltimoPeriodoMenstrual = dataUltimoPeriodoMenstrual;
+	}
+	
+	private Calendar converterDateParaCalendar(Date data) {
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(data);
+		return calendar;
+	}
+	
+	public Date calcularDataEstimadaConcepcao() {
+		Calendar dataEstimadaConcepcao = this.converterDateParaCalendar(this.dataUltimoPeriodoMenstrual);
+		dataEstimadaConcepcao.add(Calendar.WEEK_OF_YEAR, 2);
+		return dataEstimadaConcepcao.getTime();
+	}
+	
+	public Date calcularDataEstimadaParto() {
+		Calendar dataEstimadaParto = this.converterDateParaCalendar(this.dataUltimoPeriodoMenstrual);
+		dataEstimadaParto.add(Calendar.WEEK_OF_YEAR, 40);
+		return dataEstimadaParto.getTime();
+	}
+
+}
---
+++ POO/src/com/poo/gravidez/Principal.java
@@ -0,0 +1,48 @@
+package com.poo.gravidez;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String args[]) {
+		new Principal();
+	}
+	
+	public Principal() {
+		Scanner entrada = new Scanner(System.in);
+
+		try {
+			System.out.println("Data do último período menstrual (dd/mm/aaaa):");
+			String ultimoPeriodoMenstrual = entrada.nextLine();
+
+			Date dataUltimoPeriodoMenstrual = this.converterEmData(ultimoPeriodoMenstrual);
+			CalculadoraGravidez calculadora = new CalculadoraGravidez(dataUltimoPeriodoMenstrual);
+
+			Date dataEstimadaConcepcao = calculadora.calcularDataEstimadaConcepcao();
+			System.out.println("Data estimada da concepção: " 
+				+ this.formatarData(dataEstimadaConcepcao));
+
+			Date dataEstimadaParto = calculadora.calcularDataEstimadaParto();
+			System.out.println("Data estimada para parto: " 
+				+ this.formatarData(dataEstimadaParto));			
+		} catch (ParseException pe) {
+			System.out.println("Informe uma data no padrão dd/mm/aaaa.");
+		}
+	}
+	
+	private String formatarData(Date data) {
+		DateFormat formatador = new SimpleDateFormat("EEEE, dd 'de' MMMM 'de' yyyy", 
+			Locale.of("pt", "br"));
+		return formatador.format(data);
+	}
+	
+	private Date converterEmData(String texto) throws ParseException {
+		DateFormat conversor = new SimpleDateFormat("dd/MM/yyyy");
+		return conversor.parse(texto);
+		// implementar conversão de texto para data no formato dd/MM/yyyy
+	}
+}
