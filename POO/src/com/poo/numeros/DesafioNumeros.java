6bc385efe2b1f15f2ccad771f3f7556e4f13ee5a
Lucas <lucasmoraesostemberg761@gmail.com>
2026-02-11 16:58:04 -0400

    featr: desafio numeros

---
+++ POO/src/com/poo/numeros/DesafioNumeros.java
@@ -0,0 +1,23 @@
+package com.poo.numeros;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class DesafioNumeros {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		DecimalFormat formatador = new DecimalFormat("R$ #,##0.00");
+
+		double numero = entrada.nextDouble();
+		
+		BigDecimal bg = new BigDecimal(numero);
+		
+		BigDecimal percentual = bg.multiply(BigDecimal.TEN).divide(new BigDecimal(100));
+		
+		String valorFormatado = formatador.format(percentual.doubleValue());
+		
+		System.out.println(valorFormatado);
+	}
+}
